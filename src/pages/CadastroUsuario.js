4a6233549cc13d3f3573e9948884f990227f63c3
CaiqueGomesDaSilva <caique.gsilva@sptech.school>
2023-11-24 01:40:06 -0300

    validacoes do usuario

--- src/pages/CadastroUsuario.js
+++ src/pages/CadastroUsuario.js
@@ -6,101 +6,129 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 function CadastroUsuario() {
-    const [mostrarSegundaParteCad, setMostrarSegundaParteCad] = useState(false);
-    const [dadosPrimeiraParte, setDadosPrimeiraParte] = useState(null);
+  const [mostrarSegundaParteCad, setMostrarSegundaParteCad] = useState(false);
+  const [dadosPrimeiraParte, setDadosPrimeiraParte] = useState(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const cadastrarDois = (e) => {
-        e.preventDefault();
+  const cadastrarDois = (e) => {
+    e.preventDefault();
 
-        
+    // Dados da segunda parte
+    const dadosSegundaParteLocal = {
+      email: e.target.email ? e.target.email.value : '',
+      cpf: e.target.cpf ? e.target.cpf.value : '',
+      senha: e.target.senha ? e.target.senha.value : '',
+      confirmacaoSenha: e.target.confirmacao_de_senha ? e.target.confirmacao_de_senha.value : ''
+    };
 
-        // Dados da segunda parte
-        const dadosSegundaParteLocal = {
-            email: e.target.email ? e.target.email.value : '',
-            cpf: e.target.cpf ? e.target.cpf.value : '',
-            senha: e.target.senha ? e.target.senha.value : '',
-            confirmacaoSenha: e.target.confirmacao_de_senha ? e.target.confirmacao_de_senha.value : ''
-        };
+        // Validar se todos os campos estão preenchidos
+        const camposPreenchidos = Object.values(dadosSegundaParteLocal).every(value => value.trim() !== '');
+  
+        if (!camposPreenchidos) {
+          toast.error("Por favor, preencha todos os campos.");
+          return;
+        }
 
-        // Unir os dados das duas partes
-        const cadastroCompleto = {
-            ...dadosPrimeiraParte,
-            ...dadosSegundaParteLocal
-        };
+        // Validar a senha
+        const senhaValida = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(dadosSegundaParteLocal.senha);
+  
+        // Validar se a senha e a confirmação de senha são iguais
+        if (!senhaValida) {
+          toast.error("A senha deve ter pelo menos uma letra maiúscula, uma letra minúscula, um número e ter no mínimo 8 caracteres.");
+          return;
+        }
+      
+        if (dadosSegundaParteLocal.senha !== dadosSegundaParteLocal.confirmacaoSenha) {
+          toast.error("A senha e a confirmação de senha devem ser iguais.");
+          return;
+        }
+      
 
-        console.log(cadastroCompleto);
+    // Unir os dados das duas partes
+    const cadastroCompleto = {
+      ...dadosPrimeiraParte,
+      ...dadosSegundaParteLocal
+    };
 
-        api.post("/", cadastroCompleto)
-        .then((res) => {
-          // Cadastro bem-sucedido
-          Swal.fire({
-            title: 'Cadastro bem-sucedido!',
-            text: 'Deseja fazer login agora?',
-            icon: 'success',
-            showCancelButton: true,
-            confirmButtonText: 'Sim',
-            cancelButtonText: 'Não',
-          }).then((result) => {
-            if (result.isConfirmed) {
-              // Redirecionar para a página de login ou executar a lógica de login
-              navigate('/')
-            } else {
-              // Lógica a ser executada se o usuário escolher "Não"
-              navigate('/')
-            }
-          });
-        })
-        .catch((erro) => {
-          // Erro no cadastro
-          toast.error("Erro ao cadastrar!");
+    console.log(cadastroCompleto);
+
+    api.post("/", cadastroCompleto)
+      .then((res) => {
+        // Cadastro bem-sucedido
+        Swal.fire({
+          title: 'Cadastro bem-sucedido!',
+          text: 'Deseja fazer login agora?',
+          icon: 'success',
+          showCancelButton: true,
+          confirmButtonText: 'Sim',
+          cancelButtonText: 'Não',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Redirecionar para a página de login ou executar a lógica de login
+            navigate('/')
+          } else {
+            // Lógica a ser executada se o usuário escolher "Não"
+            navigate('/')
+          }
         });
+      })
+      .catch((erro) => {
+        // Erro no cadastro
+        toast.error("Erro ao cadastrar!");
+      });
+  };
+
+  const cadastrarUm = (e) => {
+    e.preventDefault();
+
+    // Dados da primeira parte
+    const dadosPrimeiraParte = {
+      nome: e.target.nome ? e.target.nome.value : '',
+      genero: e.target.genero ? e.target.genero.value : '',
+      orientacaoSexual: e.target.orientacao_sexual ? e.target.orientacao_sexual.value : ''
     };
+    console.log(dadosPrimeiraParte)
 
-    const cadastrarUm = (e) => {
-        e.preventDefault();
-
-        // Dados da primeira parte
-        const dadosPrimeiraParte = {
-            nome: e.target.nome ? e.target.nome.value : '',
-            genero: e.target.genero ? e.target.genero.value : '',
-            orientacaoSexual: e.target.orientacao_sexual ? e.target.orientacao_sexual.value : ''
-        };
-        console.log(dadosPrimeiraParte)
-
-        // Armazenar os dados
-        setDadosPrimeiraParte(dadosPrimeiraParte);
-
-        // Mostrar a segunda parte
-        handleContinuarClick();
-    };
-
-    const handleContinuarClick = () => {
-        setMostrarSegundaParteCad(true);
-    };
+    // Verificar se todos os campos estão preenchidos
+    if (Object.values(dadosPrimeiraParte).some(value => value === '')) {
+      toast.error("Todos os campos do cadastro devem ser preenchidos.");
+      return;
+    }
 
 
-    return (
-        <Cadastro
-            MTbarraUm={"92.77px"}
-            MBbarraUm={"80px"}
-            inputTitlesUm={['Nome', 'Gênero', 'Orientação Sexual']}
-            tituloUm="Seja Membro!"
-            subtituloUm="Faça parte do projeto e aproveite benefícios exclusivos."
-            tituloBotaoUm="Continuar"
+    // Armazenar os dados
+    setDadosPrimeiraParte(dadosPrimeiraParte);
 
-            MTbarraDois={"60.77px"}
-            MBbarraDois={"80px"}
-            inputTitlesDois={['Email', 'Cpf', 'Senha', 'Confirmação de senha']}
-            tituloDois="Seja Membro"
-            tituloBotaoDois="Confirmar"
-            subtituloDois="Faça parte do projeto e aproveite benefícios exclusivos."
-            mostrarSegundaParteCad={mostrarSegundaParteCad}
-            cadastrarDois={cadastrarDois}
-            cadastrarUm={cadastrarUm}
-        />
-    )
+    // Mostrar a segunda parte
+    handleContinuarClick();
+  };
+
+  const handleContinuarClick = () => {
+    setMostrarSegundaParteCad(true);
+  };
+
+
+  return (
+    <Cadastro
+      MTbarraUm={"92.77px"}
+      MBbarraUm={"80px"}
+      inputTitlesUm={['Nome', 'Gênero', 'Orientação Sexual']}
+      tituloUm="Seja Membro!"
+      subtituloUm="Faça parte do projeto e aproveite benefícios exclusivos."
+      tituloBotaoUm="Continuar"
+
+      MTbarraDois={"60.77px"}
+      MBbarraDois={"80px"}
+      inputTitlesDois={['Email', 'Cpf', 'Senha', 'Confirmação de senha']}
+      tituloDois="Seja Membro"
+      tituloBotaoDois="Confirmar"
+      subtituloDois="Faça parte do projeto e aproveite benefícios exclusivos."
+      mostrarSegundaParteCad={mostrarSegundaParteCad}
+      cadastrarDois={cadastrarDois}
+      cadastrarUm={cadastrarUm}
+    />
+  )
 }
 
 
