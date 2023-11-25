e573b7829670253a62cd71e8e7fca9713adb278f
shadowbmo <douglas.silva@sptech.school>
2023-11-24 21:30:05 -0300

    Mergeado tela do joao

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import Map from './pages/TelaExplorador';
 import Eventos from "./components/TelasUsuario/Eventos"
 import Avaliacoes from "./components/TelasUsuario/Avaliacoes"
 import HomeEventos from './components/TelasUsuario/Home-eventos';
+import EventosEmpresa from "./pages/EventosEmpresa";
 
 
 
@@ -24,6 +25,8 @@ const Rotas = () => {
         <Route path="/eventos" element={<Eventos />} />
         <Route path="/avaliacoes" element={<Avaliacoes />} />
         <Route path="/home-eventos" element={<HomeEventos />} />
+        <Route path="*" element={<h1>Not Found 404</h1>} /> 
+        <Route path="/eventos-empresa" element={<EventosEmpresa />} />
       </Routes>
     </Router>
    )
