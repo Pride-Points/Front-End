ade5703fb0d87af3eabd4d57773cfab3e6305e6e
CaiqueGomesDaSilva <caique.gsilva@sptech.school>
2024-05-07 20:50:32 -0300

    ajuste nas rotas

--- src/components/TelasUsuario/PopUp/popUpAvaliacoes.jsx
+++ src/components/TelasUsuario/PopUp/popUpAvaliacoes.jsx
@@ -1,18 +1,15 @@
+import "./popUpAvaliacoes.css";
+import fecharBar from "../../../assets/Fechar.png";
+import imagemPerfil from "../../../assets/PESOSOSOA.svg";
+import ModalAvaliacao from "../Modal/modal"; // Importe o componente
+import { toast } from "react-toastify";
+import api from "../../../api/api";
+import StarRating from "./estrelas";
 
-import "./popUpAvaliacoes.css"
-import fecharBar from '../../../assets/Fechar.png';
-import imagemPerfil from '../../../assets/PESOSOSOA.svg'
-import ModalAvaliacao from '../Modal/modal'; // Importe o componente
-import { toast } from 'react-toastify';
-import api from '../../../api/api';
-import StarRating from "./estrelas"
-
-
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Importe o componente Link do React Router
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // Importe o componente Link do React Router
 
 function PopUp() {
-
   const [empresaDetalhes, setEmpresaDetalhes] = useState(null);
   const [avaliacoes, setAvaliacoes] = useState(null);
   const [nota2, setNota2] = useState(null);
@@ -20,13 +17,12 @@ function PopUp() {
   useEffect(() => {
     const buscarEmpresaPorId = async (token) => {
       try {
-        token = sessionStorage.authToken
+        token = sessionStorage.authToken;
 
-
-        const idEmpresa = sessionStorage.getItem('idEmpresaClicada');
+        const idEmpresa = sessionStorage.getItem("idEmpresaClicada");
 
         if (!idEmpresa) {
-          throw new Error('ID da empresa não encontrado no sessionStorage');
+          throw new Error("ID da empresa não encontrado no sessionStorage");
         }
 
         const response = await api.get(`/empresas/${idEmpresa}`, {
@@ -36,13 +32,14 @@ function PopUp() {
         });
 
         if (response.status === 200 && response.data) {
-
           setEmpresaDetalhes(response.data);
         } else {
-          throw new Error('Ops! Ocorreu um erro ao buscar os detalhes da empresa.');
+          throw new Error(
+            "Ops! Ocorreu um erro ao buscar os detalhes da empresa."
+          );
         }
       } catch (error) {
-        console.error('Erro ao buscar detalhes da empresa:', error);
+        console.error("Erro ao buscar detalhes da empresa:", error);
         toast.error(error.message);
       }
     };
@@ -53,13 +50,12 @@ function PopUp() {
   useEffect(() => {
     const buscarAvaliacoes = async (token) => {
       try {
-        token = sessionStorage.authToken
+        token = sessionStorage.authToken;
 
-
-        const idEmpresa = sessionStorage.getItem('idEmpresaClicada');
+        const idEmpresa = sessionStorage.getItem("idEmpresaClicada");
 
         if (!idEmpresa) {
-          throw new Error('ID da empresa não encontrado no sessionStorage');
+          throw new Error("ID da empresa não encontrado no sessionStorage");
         }
 
         const response = await api.get(`/avaliacoes/${idEmpresa}`, {
@@ -71,10 +67,12 @@ function PopUp() {
         if (response.status === 200 && response.data) {
           setAvaliacoes(response.data);
         } else {
-          throw new Error('Ops! Ocorreu um erro ao buscar os detalhes da empresa.');
+          throw new Error(
+            "Ops! Ocorreu um erro ao buscar os detalhes da empresa."
+          );
         }
       } catch (error) {
-        console.error('Erro ao buscar detalhes da empresa:', error);
+        console.error("Erro ao buscar detalhes da empresa:", error);
         toast.error(error.message);
       }
     };
@@ -86,13 +84,12 @@ function PopUp() {
     const buscarNotas = async (token) => {
       try {
         const userId = sessionStorage.id; // Substitua pelo ID do usuário que você quer buscar as avaliações
-        token = sessionStorage.authToken
+        token = sessionStorage.authToken;
 
-
-        const idEmpresa = sessionStorage.getItem('idEmpresaClicada');
+        const idEmpresa = sessionStorage.getItem("idEmpresaClicada");
 
         if (!idEmpresa) {
-          throw new Error('ID da empresa não encontrado no sessionStorage');
+          throw new Error("ID da empresa não encontrado no sessionStorage");
         }
 
         const response = await api.get(`/empresas/media/${idEmpresa}`, {
@@ -104,10 +101,12 @@ function PopUp() {
         if (response.status === 200 && response.data) {
           setNota2(response.data);
         } else {
-          throw new Error('Ops! Ocorreu um erro ao buscar os detalhes da empresa.');
+          throw new Error(
+            "Ops! Ocorreu um erro ao buscar os detalhes da empresa."
+          );
         }
       } catch (error) {
-        console.error('Erro ao buscar detalhes da empresa:', error);
+        console.error("Erro ao buscar detalhes da empresa:", error);
         toast.error(error.message);
       }
     };
@@ -118,91 +117,96 @@ function PopUp() {
   let abrirModal = () => {
     setModalAberto(true);
 
-
-  let fecharModal = () => {
-    setModalAberto(false);
-
-  };
-  return (
-
-    <div className="popUp">
-      <div className="containerBar">
-        <div className="nomeBar">
-          {empresaDetalhes && empresaDetalhes.nomeFantasia}
-          <span className="notaBar">
-          <StarRating className="DgEstrela" rating={nota2} />
-          </span>
-        </div>
-        <Link to="/home-usuario" className="fecharBar">
-          <img src={fecharBar} alt="Botão para fechar o estabelecimento e ir para a tela inicial do mapa" />
-        </Link>
-      </div>
-      <div className="containerDescricao">
-        <div className="descricaoBar">
-
-        </div>
-      </div>
-      <div className="containerInformacoes">
-        <div>
-          Localizado em: <span>{empresaDetalhes && empresaDetalhes.cidade},  Nº{empresaDetalhes && empresaDetalhes.numero}</span>
-        </div>
-        <div>
-          Horário de funcionamento: <span>18:00 - 05:00</span>
-        </div>
-      </div>
-      <div className="botaoAvalie">
-        <button onClick={abrirModal}>Avalie</button>
-        <ModalAvaliacao onClose={fecharModal} mostrarModal={modalAberto} />
-      </div>
-      <div className="containerOpcoes">
-        <div className="opcoes selecionada">
-          <Link to="/home-usuario-avaliacoes" style={{ textDecoration: 'none', color: 'black' }}>Avalições</Link>
-
-        </div>
-        <div className="opcoes">
-          <Link to="/home-usuario-eventos" style={{ textDecoration: 'none', color: 'black' }}>Eventos</Link>
-        </div>
-      </div>
-      <div className="containesLocaisE">
-        {/* Aqui você vai mapear as avaliações e exibi-las */}
-        {avaliacoes && avaliacoes.map((avaliacao) => (
-          <div className="containerLocalE" key={avaliacao.id}>
-            <div className="containerImagem">
-              <img src={imagemPerfil} alt="" />
-              <div className="resposta">
-                <div className="resposta">
-
-                </div>
-
-              </div>
-            </div>
-            <div className="containerLocalDireita">
-              <div className="containerLocalCima">
-                <div className="tituloNome">
-                  {avaliacao.nomeAvaliador}
-                </div>
-                <div className="estrelasEvento">
-                  {/* Renderizando as estrelas com base na nota da avaliação */}
-                  <StarRating className="DgEstrela" rating={avaliacao.nota} />
-               
-                  </div>
-              </div>
-              <div className="descricaoAvaliacao">
-                {avaliacao.comentario}
-              </div>
-              <div className="avaliacaoData">
-                {avaliacao.dtAvaliacao}
-              </div>
-            </div>
-            {/* Adicione o restante das informações da avaliação */}
+    let fecharModal = () => {
+      setModalAberto(false);
+    };
+    return (
+      <div className="popUp">
+        <div className="containerBar">
+          <div className="nomeBar">
+            {empresaDetalhes && empresaDetalhes.nomeFantasia}
+            <span className="notaBar">
+              <StarRating className="DgEstrela" rating={nota2} />
+            </span>
           </div>
-        ))}
-
+          <Link to="/home-usuario" className="fecharBar">
+            <img
+              src={fecharBar}
+              alt="Botão para fechar o estabelecimento e ir para a tela inicial do mapa"
+            />
+          </Link>
+        </div>
+        <div className="containerDescricao">
+          <div className="descricaoBar"></div>
+        </div>
+        <div className="containerInformacoes">
+          <div>
+            Localizado em:{" "}
+            <span>
+              {empresaDetalhes && empresaDetalhes.cidade}, Nº
+              {empresaDetalhes && empresaDetalhes.numero}
+            </span>
+          </div>
+          <div>
+            Horário de funcionamento: <span>18:00 - 05:00</span>
+          </div>
+        </div>
+        <div className="botaoAvalie">
+          <button onClick={abrirModal}>Avalie</button>
+          <ModalAvaliacao onClose={fecharModal} mostrarModal={modalAberto} />
+        </div>
+        <div className="containerOpcoes">
+          <div className="opcoes selecionada">
+            <Link
+              to="/home-usuario-avaliacoes"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Avalições
+            </Link>
+          </div>
+          <div className="opcoes">
+            <Link
+              to="/home-usuario-eventos"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Eventos
+            </Link>
+          </div>
+        </div>
+        <div className="containesLocaisE">
+          {/* Aqui você vai mapear as avaliações e exibi-las */}
+          {avaliacoes &&
+            avaliacoes.map((avaliacao) => (
+              <div className="containerLocalE" key={avaliacao.id}>
+                <div className="containerImagem">
+                  <img src={imagemPerfil} alt="" />
+                  <div className="resposta">
+                    <div className="resposta"></div>
+                  </div>
+                </div>
+                <div className="containerLocalDireita">
+                  <div className="containerLocalCima">
+                    <div className="tituloNome">{avaliacao.nomeAvaliador}</div>
+                    <div className="estrelasEvento">
+                      {/* Renderizando as estrelas com base na nota da avaliação */}
+                      <StarRating
+                        className="DgEstrela"
+                        rating={avaliacao.nota}
+                      />
+                    </div>
+                  </div>
+                  <div className="descricaoAvaliacao">
+                    {avaliacao.comentario}
+                  </div>
+                  <div className="avaliacaoData">{avaliacao.dtAvaliacao}</div>
+                </div>
+                {/* Adicione o restante das informações da avaliação */}
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
-
-  );
-}
+    );
+  };
 }
 
 export default PopUp;
--- src/routes.js
+++ src/routes.js
@@ -1,48 +1,51 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import CadastroUsuario from "./pages/CadastroUsuario";
 import CadastroEmpresa from "./pages/CadastroEmpresa";
 import Login from "./components/Login/Login";
-import Map from './pages/TelaExplorador';
-import AvaliacaoEmpresa from './pages/AvaliacaoEmpresa';
-import Eventos from "./components/TelasUsuario/Eventos"
+import Map from "./pages/TelaExplorador";
+import AvaliacaoEmpresa from "./pages/AvaliacaoEmpresa";
+import Eventos from "./components/TelasUsuario/Eventos";
 import EventosEmpresa from "./pages/EventosEmpresa";
-import Avaliacoes from "./components/TelasUsuario/Avaliacoes"
+import Avaliacoes from "./components/TelasUsuario/Avaliacoes";
 import Dash from "./components/dashboard/dash.jsx";
-import Funcionarios from "./pages/Funcionarios"
+import Funcionarios from "./pages/Funcionarios";
 import HomeLocais from "./pages/Home";
-import HomeLocaisAvalicoes from "./components/TelasUsuario/Home-avaliacoes"
-import HomeLocaisEventos from "./components/TelasUsuario/Home-eventos.jsx"
-import HomeUsuarioEventos from "./components/TelasUsuario/Home-locais-eventos.jsx"
-
-
+import HomeLocaisAvalicoes from "./components/TelasUsuario/Home-avaliacoes";
+import HomeLocaisEventos from "./components/TelasUsuario/Home-eventos.jsx";
+import HomeUsuarioEventos from "./components/TelasUsuario/Home-locais-eventos.jsx";
 
 const Rotas = () => {
-   return(
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/eventos-empresa" element={<EventosEmpresa />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
-        <Route path="/cadastro-empresa" element={<CadastroEmpresa />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/explorador" element={<Map />} /> 
+        <Route path="/cadastro-empresa" element={<CadastroEmpresa />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/explorador" element={<Map />} />
         <Route path="/avaliacao-empresa" element={<AvaliacaoEmpresa />} />
         <Route path="/eventos" element={<Eventos />} />
         <Route path="/avaliacoes" element={<Avaliacoes />} />
-        <Route path="*" element={<h1>Not Found 404</h1>} /> 
         <Route path="/dash" element={<Dash />} />
         <Route path="/funcionarios" element={<Funcionarios />} />
         <Route path="/home-usuario" element={<HomeLocais />} />
-        <Route path="/home-usuario-avaliacoes" element={<HomeLocaisAvalicoes />} />
+        <Route
+          path="/home-usuario-avaliacoes"
+          element={<HomeLocaisAvalicoes />}
+        />
         <Route path="/home-usuario-eventos" element={<HomeLocaisEventos />} />
-        <Route path="/home-usuario-eventos-locais" element={<HomeUsuarioEventos />} />
-          </Routes>
+        <Route
+          path="/home-usuario-eventos-locais"
+          element={<HomeUsuarioEventos />}
+        />
+        <Route path="*" element={<h1>Not Found 404</h1>} />
+      </Routes>
     </Router>
-   )
-}
+  );
+};
 
 export default Rotas;
