adf01981e8f79a0201278531862f0e7126261cfe
CaiqueGomesDaSilva <caique.gsilva@sptech.school>
2024-05-07 21:48:43 -0300

    ajuste v1

--- src/components/TelasUsuario/Modal/modal.jsx
+++ src/components/TelasUsuario/Modal/modal.jsx
@@ -3,6 +3,7 @@ import closeModal from '../../../assets/closeModal.svg'
 import estrela from '../../../assets/Starrrrr.svg'
 import estrelaP from '../../../assets/starrrpreenchida.svg'
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 import api from '../../../api/api'
 
@@ -13,6 +14,7 @@ function ModalAvaliacao({ onClose, mostrarModal, modoEdicao = false, avaliacaoPa
     const [selectedEmotion, setSelectedEmotion] = useState(modoEdicao ? avaliacaoParaEditar.tag : '');
     const [estrelasHover, setEstrelasHover] = useState(modoEdicao ? avaliacaoParaEditar.nota : 0);
     const [comentario, setComentario] = useState(modoEdicao ? avaliacaoParaEditar.comentario : '');
+    const navigate = useNavigate();
 
     const handleMouseEnter = (id) => {
         setEstrelasHover(id);
@@ -48,43 +50,44 @@ function ModalAvaliacao({ onClose, mostrarModal, modoEdicao = false, avaliacaoPa
     const dataAvaliacao = `${ano}-${mes}-${dia}`;
 
 
-    const enviarAvaliacao = async () => {
-        try {
-            const usuarioId = sessionStorage.id;
-            const empresaId = sessionStorage.idEmpresaClicada;
-            const token = sessionStorage.authToken;
-            const data = {
-                nota: estrelasHover,
-                tag: selectedEmotion,
-                comentario,
-                dtAvaliacao: dataAvaliacao, // Considerar se a data deve ser atualizada na edição
-            };
-            const config = {
-                headers: { Authorization: `Bearer ${token}` }
-            };
-    
-            const url = modoEdicao ?
-                `/avaliacoes/${avaliacaoParaEditar.id}/${usuarioId}/${empresaId}` :
-                `/avaliacoes/${empresaId}/${usuarioId}`;
-    
-            const metodoHttp = modoEdicao ? api.put : api.post;
-    
-            const response = await metodoHttp(url, data, config);
+    const enviarAvaliacao = () => {
+        const usuarioId = sessionStorage.id;
+        const empresaId = sessionStorage.idEmpresaClicada;
+        const token = sessionStorage.authToken;
+        const data = {
+            nota: estrelasHover,
+            tag: selectedEmotion,
+            comentario,
+            dtAvaliacao: dataAvaliacao, // Considerar se a data deve ser atualizada na edição
+        };
+        const config = {
+            headers: { Authorization: `Bearer ${token}` }
+        };
 
-            if(response.status !== 200){
-                toast.error('"Erro ao enviar avaliação. Por favor, tente novamente mais tarde."')
-                return;
-              }
-            toast.success('Avaliação enviada com sucesso!', response.data);
-            window.location.reload();
-            onAvaliacaoSalva(); // Callback para atualizar a lista de avaliações no componente pai
-        } catch (error) {
-            console.error('Erro ao enviar avaliação:', error);
-        }
-    };
-    
-    enviarAvaliacao();
-    
+
+        const url = modoEdicao ? 
+            `/avaliacoes/${avaliacaoParaEditar.id}/${usuarioId}/${empresaId}` :
+            `/avaliacoes/${empresaId}/${usuarioId}`;
+
+        const metodoHttp = modoEdicao ? api.put : api.post;
+
+        metodoHttp(url, data, config)
+            .then(response => {
+
+                if(response.status !== 200){
+                    toast.error('"Erro ao enviar avaliação. Por favor, tente novamente mais tarde."')
+                    return;
+                  }
+                toast.success('Avaliação enviada com sucesso!', response.data);
+                navigate('/home-usuario-avaliacoes')
+
+                onAvaliacaoSalva(); // Callback para atualizar a lista de avaliações no componente pai
+
+            })
+            .catch(error => {
+                console.error('Erro ao enviar avaliação:', error);
+            });
+        };
 
 
     return (
